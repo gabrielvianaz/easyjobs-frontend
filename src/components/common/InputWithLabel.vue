8cafe6605f66f7e5d161e2316fba3ed79165de84
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-09 15:48:00 -0300

    Criado componente LoadingButton e arquivo main.css, mudanças no componente InputWithLabel

--- src/components/common/InputWithLabel.vue
+++ src/components/common/InputWithLabel.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="input-with-label">
-    <label for="senha">{{ label }}</label>
-    <input
-      :class="class"
-      :type="type"
-      :name="name"
-      :id="id"
-      :placeholder="placeholder"
-      :value="modelValue"
-      @input="modelUpdate"
-    />
+    <label :for="id">{{ label }}</label>
+    <div class="input-wrapper" :class="class">
+      <input
+        :type="type"
+        :name="name"
+        :id="id"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="modelUpdate"
+      />
+    </div>
   </div>
 </template>
 
@@ -36,6 +37,40 @@ label {
   font-weight: 600;
 }
 
+.input-with-label.erro label {
+  color: red;
+}
+
+.input-wrapper {
+  display: flex;
+}
+
+.input-wrapper::before {
+  position: absolute;
+  display: block;
+  content: '';
+  width: 29px;
+  height: 40px;
+  background-repeat: no-repeat;
+  background-position: 13px center;
+  background-size: 16px 16px;
+}
+
+.input-wrapper.email::before {
+  background-image: url('@/assets/img/icons/email.svg');
+}
+
+.input-wrapper.senha::before {
+  background-image: url('@/assets/img/icons/senha.svg');
+}
+
+.input-with-label.erro .input-wrapper.email::before {
+  background-image: url('@/assets/img/icons/email-erro.svg');
+}
+.input-with-label.erro .input-wrapper.senha::before {
+  background-image: url('@/assets/img/icons/senha-erro.svg');
+}
+
 input {
   width: 100%;
   height: 40px;
@@ -43,18 +78,12 @@ input {
   border-radius: 5px;
   font-size: 1rem;
   color: #f97c09;
-  background-repeat: no-repeat;
-  background-position: 10px center;
-  background-size: 16px 16px;
-  padding: 0 36px;
+  padding-left: 35px;
 }
 
-input.email {
-  background-image: url('@/assets/img/icons/email.svg');
-}
-
-input.senha {
-  background-image: url('@/assets/img/icons/senha.svg');
+.input-with-label.erro input {
+  border-color: red;
+  color: red;
 }
 
 input:focus {
@@ -63,5 +92,23 @@ input:focus {
 
 ::placeholder {
   color: #fcddc1;
+}
+
+.input-with-label.erro ::placeholder {
+  color: #ffb2b2;
+}
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus {
+  -webkit-box-shadow: 0 0 0px 1000px #fff inset;
+  -webkit-text-fill-color: #f97c09;
+  caret-color: #f97c09;
+}
+
+.input-with-label.erro input:-webkit-autofill,
+.input-with-label.erro input:-webkit-autofill:hover,
+.input-with-label.erro input:-webkit-autofill:focus {
+  -webkit-text-fill-color: red;
 }
 </style>
---
+++ src/components/common/LoadingButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="" disabled>
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    Carregando...
+  </button>
+</template>
+
+<style scoped>
+button {
+  width: 100%;
+  height: 40px;
+  background-color: #fb9d47 !important;
+  border: none;
+  color: #fff !important;
+  border-radius: 5px;
+  font-weight: 600;
+}
+
+span.spinner-border {
+  color: #fff !important;
+}
+
+button:focus,
+button:hover {
+  outline: none;
+}
+</style>

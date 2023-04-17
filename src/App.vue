b0b4f835dfea351f42a9a38d30f268f78960c8ac
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-16 21:43:31 -0300

    Utilização da função setUsuario com o hook onBeforeMount no arquivo 'App.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
   <RouterView />
 </template>
+
+<script lang="ts" setup>
+import { onBeforeMount } from 'vue'
+import setUsuario from './functions/setUsuario'
+
+onBeforeMount(() => setUsuario())
+</script>

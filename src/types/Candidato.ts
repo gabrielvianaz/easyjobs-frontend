96396658faf8b696b5fa0f1e1ce53a9cfe5a8089
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-16 22:02:07 -0300

    Implementados tipos Candidato, Cidade, Empresa e Estado

---
+++ src/types/Candidato.ts
@@ -0,0 +1,8 @@
+export type Candidato = {
+  nome: string
+  cpf: string
+  dataNascimento: string
+  genero: string
+  cidade: string
+  estado: string
+}
---
+++ src/types/Empresa.ts
@@ -0,0 +1,8 @@
+export type Empresa = {
+  razaoSocial: string
+  nomeFantasia: string
+  cnpj: string
+  dataFundacao: string
+  cidade: string
+  estado: string
+}
